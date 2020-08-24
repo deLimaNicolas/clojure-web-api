b709eaf2f408e61e1e0c1c77e7520c2656ea0170
Nicolas Vinicius Teixeira de Lima <nicolas.lima@mundiale.com.br>
2020-08-24 11:35:49 -0300

    webpage basic structure

--- webapp/src/webapp/core.clj
+++ webapp/src/webapp/core.clj
@@ -1,6 +1,36 @@
-(ns webapp.core)
+(ns webapp.core
+  (:require [net.cgrand.enlive-html :as enlive]
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Tweed [title content])
+
+(defprotocol TweedStore
+  (get-tweeds [store])
+  (put-tweed! [store tweed]))
+
+(defrecord AtomStore [data])
+
+(extend-protocol TweedStore
+  AtomStore
+  (get-tweeds [store]
+    (get @(:data store) :tweeds))
+  (put-tweed! [store tweed]
+    (swap! (:data store)
+           update-in [:tweeds] conj tweed)))
+
+(def store (->AtomStore (atom {:tweeds '()})))
+
+(enlive/defsnippet tweed-tpl  "../resources/tweedler/index.html" [[:article.tweed enlive/first-of-type]]
+  [tweed]
+  [:.title] (enlive/html-content (:title tweed))
+  [:.content] (enlive/html-content (:content tweed)))
+
+(enlive/deftemplate index-tpl "../resources/tweedler/index.html"
+  [tweeds]
+  [:section.tweeds] (enlive/content (map tweed-tpl tweeds)))
+
+(defroutes app
+  (GET "/" [] (index-tpl (get-tweeds store))))
+
+(def server (jetty/run-jetty app {:port 3000 :join? false}))
